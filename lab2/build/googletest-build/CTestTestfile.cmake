0db83055c33a2a30588615bbfe202fd22eaa9779
arslen08 <arslandodonov2@gmail.com>
2025-10-11 02:06:36 +0300

    Delete lab2 directory

--- lab2/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seaspray/projects/helloworld/OOP-labs/googletest
-# Build directory: /home/seaspray/projects/helloworld/OOP-labs/lab2/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- lab2/build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seaspray/projects/helloworld/OOP-labs/googletest/googletest
-# Build directory: /home/seaspray/projects/helloworld/OOP-labs/lab2/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
